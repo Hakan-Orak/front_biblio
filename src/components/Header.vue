4b8570d7423423897e72be74cdc0acae81d17251
PLekraie <pauline.lekraie@gmail.com>
2021-04-14 18:05:34 +0200

    feat(Homepage): add header and first possible rendition of images

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="header">
+		<h2>Nom de l'appli</h2>
+		<div id="nav">
+			<router-link to="/">Images</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#header {
+	height: 10vh;
+	min-height: 100px;
+	width: 100%;
+	box-shadow: 0px 5px 10px rgb(221, 221, 221);
+	display: flex;
+	justify-content: space-evenly;
+	align-items: baseline;
+	margin-bottom: 2rem;
+}
+#nav {
+	padding: 30px;
+}
+
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
+	text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+	color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div>
+		<input type="text" placeholder="Rechercher une image" />
+		<div class="imgList">
+			<div v-for="img in tempImgList" :key="img.id" class="imgBlock">
+				<img :src="img.link" class="imgPreview" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+	data() {
+		return {
+			tempImgList: [
+				{ id: 1, link: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 2, link: 'https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80' },
+				{ id: 3, link: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 4, link: 'https://images.unsplash.com/photo-1587783778110-2e4c56c5767e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' },
+				{ id: 5, link: 'https://images.unsplash.com/photo-1560807310-21f376a29d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHJhbmRvbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+				{ id: 18, link: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 6, link: 'https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80' },
+				{ id: 7, link: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 8, link: 'https://images.unsplash.com/photo-1587783778110-2e4c56c5767e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' },
+				{ id: 9, link: 'https://images.unsplash.com/photo-1560807310-21f376a29d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHJhbmRvbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+				{ id: 19, link: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 10, link: 'https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80' },
+				{ id: 11, link: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 12, link: 'https://images.unsplash.com/photo-1587783778110-2e4c56c5767e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' },
+				{ id: 13, link: 'https://images.unsplash.com/photo-1560807310-21f376a29d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHJhbmRvbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+				{ id: 20, link: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 14, link: 'https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80' },
+				{ id: 15, link: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
+				{ id: 16, link: 'https://images.unsplash.com/photo-1587783778110-2e4c56c5767e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' },
+				{ id: 17, link: 'https://images.unsplash.com/photo-1560807310-21f376a29d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHJhbmRvbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' }
+			]
+		};
+	},
+	components: {
+		HelloWorld
+	}
 };
 </script>
+
+<style scoped>
+.imgList {
+	display: flex;
+	flex-wrap: wrap;
+}
+.imgBlock {
+	padding: 0.5rem;
+	margin: 1rem;
+	box-shadow: 0px 5px 10px rgb(221, 221, 221);
+}
+.imgPreview {
+	max-width: 20vw;
+	max-height: 20vh;
+}
+
+input {
+	border: 0px;
+	box-shadow: inset 0px 2px 10px rgb(221, 221, 221), inset 0px -2px 10px rgb(221, 221, 221);
+  width: 50%;
+  height:2rem;
+  padding: 0.5rem;
+}
+</style>
